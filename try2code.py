fb66eade5490f0a6d296efc96e4c12a22ad7e7d9
Zilan Altindag <139772847+zilan1altindag@users.noreply.github.com>
2023-11-30 15:05:53 +0000

    Update try2code.py

--- try2code.py
+++ try2code.py
@@ -1,20 +1,30 @@
-import itertools
-
-def evaluate_policy(osa, osb, osc, xdqp, awvp, niqn, wigq, gusq, rtoy):
-    if osa and not osb and not osc and not xdqp and not awvp and not (niqn or wigq) and gusq and not (rtoy):
-        return True
-    else:
-        return False
+import requests
 
 def main():
-    attributes = ['OSA', 'OSB', 'OSC', 'XDGP', 'AWVP', 'NIQN', 'WIGQ', 'GUSQ', 'RTOY']
-    values = [True, False]
-    for combination in itertools.product(values, repeat=len(attributes)):
-        evaluated = evaluate_policy(*combination)
-        if evaluated:
-            print('Found a combination that evaluates to true:')
-            print(combination)
-            break
+    # Replace with the actual server address
+    server_address = ' http://10.0.0.6/ctf_deploy/aclive/ddjS7HkE9M/A.php'
+
+    # Replace with the correct combination of parameters
+    params = {
+        'osa': True,
+        'osb': False,
+        'osc': False,
+        'xdqp': False,
+        'awvp': False,
+        'niqn': False,
+        'wigq': False,
+        'gusq': True,
+        'rtoy': False
+    }
+
+    response = requests.get(server_address + '/policy/evaluate', params=params)
+
+    if response.status_code == 200:
+        print('Policy evaluated to true:')
+        print('Flag:', response.json()['flag'])
+    else:
+        print('Error evaluating policy:', response.status_code)
 
 if __name__ == '__main__':
     main()
+
